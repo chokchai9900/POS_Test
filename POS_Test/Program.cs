9ad7200cccc65072763c83cac8a785ba78fa9342
chokchai9900 <48384299+chokchai9900@users.noreply.github.com>
2019-05-28 16:50:20 +0700

    Update Program.cs

--- POS_Test/Program.cs
+++ POS_Test/Program.cs
@@ -1,12 +1,38 @@
-﻿using System;
+using System;
 
-namespace POS_Test
+namespace Pos_Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var @Interface = new @Interface();
+            var Addproduct = new Addproduct();
+            string continus = "";
+            do
+            {
+                @Interface.ModeMenu();
+                Console.Write("Enter number for Your Choose : ");
+                int input = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nYou Choose Mode " + input);
+                if (input == 1)
+                {
+                    Console.Clear();
+                    @Interface.CheckbillMenu();
+                }
+                else if (input == 2)
+                {
+                    Console.Clear();
+                    Addproduct.Add();
+                }
+                Console.Write("Do you want to continus ? (y / n)");
+                continus = Console.ReadLine();
+            } while (continus == "y");
+            Console.WriteLine("GoodBye !!");
+
+
         }
     }
 }
